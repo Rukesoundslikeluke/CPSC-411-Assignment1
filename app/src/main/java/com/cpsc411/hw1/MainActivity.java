eda177ffd8c87f1593bb92bf15b7dc892f87d875
Ruke <michael.ruke@gmail.com>
2019-09-25 00:22:33 -0700

    Finished, need to refactor

--- app/src/main/java/com/cpsc411/hw1/MainActivity.java
+++ app/src/main/java/com/cpsc411/hw1/MainActivity.java
@@ -25,41 +25,46 @@ public class MainActivity extends Activity {
                 ViewGroup.LayoutParams.WRAP_CONTENT
         );
 
-        //Main wrapping grid
+        //Main wrapping grid  5x11 cells
         GridLayout root = new GridLayout(this);
         root.setOrientation(GridLayout.VERTICAL);
         root.setLayoutParams(WW);
         root.setColumnCount(5);
         root.setRowCount(11);
 
+        //Column widths
 
+        int col1width = 60;
+        int col2width = 225;
+        int col3width = 470; // handles 3 and 4
+        int col5width = 670;
+        int titleWidth = col2width + col3width + col5width;
 
-        //Column 1
+    //Column 1 + title
         for (int i=1;i<12;i++){
             TextView x = new TextView(this);
             x.setText(java.lang.Integer.toString(i));
             x.setBackgroundColor(Color.GRAY);
             x.setGravity(Gravity.CENTER);
-            x.setMinWidth(70);
+            x.setMinWidth(col1width);
             x.setBackground(getDrawable(R.drawable.thin_border));
             root.addView(x);
         }
-    //Title bar
+        //Title bar
         GridLayout.LayoutParams gParams = new GridLayout.LayoutParams(
                 GridLayout.spec(0),
-                GridLayout.spec(1,4,ViewGroup.LayoutParams.MATCH_PARENT)
+                GridLayout.spec(1,4)
         );
         TextView title = new TextView(this);
         title.setBackgroundColor(Color.BLACK);
         title.setTextColor(Color.WHITE);
+        title.setMinWidth(titleWidth);
         title.setText("Rules void hello1(int hour)");
         title.setGravity(Gravity.CENTER);
         title.setLayoutParams(gParams);
         root.addView(title);
 
     //Column 2
-        int col2width = 250;
-
         //Grid for column 2
         GridLayout col2 = new GridLayout(this);
         col2.setOrientation(GridLayout.VERTICAL);
@@ -115,9 +120,11 @@ public class MainActivity extends Activity {
             root.addView(col2txt);
         }
 
-    //Column 3
-        int col3width = 500;
-        
+    //Column 3 + 4
+        gParams = new GridLayout.LayoutParams(
+                GridLayout.spec(1),
+                GridLayout.spec(2,2)
+        );
         TextView col3txt = new TextView(this);
         col3txt.setText("name");
         col3txt.setGravity(Gravity.CENTER);
@@ -126,15 +133,165 @@ public class MainActivity extends Activity {
         col3txt.setLayoutParams(gParams);
         root.addView(col3txt);
 
+        gParams = new GridLayout.LayoutParams(
+                GridLayout.spec(2),
+                GridLayout.spec(2,2)
+        );
+        col3txt = new TextView(this);
+        col3txt.setText("category");
+        col3txt.setGravity(Gravity.CENTER);
+        col3txt.setMinWidth(col3width);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border));
+        col3txt.setLayoutParams(gParams);
+        root.addView(col3txt);
+
+        gParams = new GridLayout.LayoutParams(
+                GridLayout.spec(3),
+                GridLayout.spec(2,2)
+        );
+        col3txt = new TextView(this);
+        col3txt.setText("C1");
+        col3txt.setGravity(Gravity.CENTER);
+        col3txt.setMinWidth(col3width);
+        col3txt.setTypeface(null, Typeface.BOLD);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col3txt.setLayoutParams(gParams);
+        root.addView(col3txt);
+
+        gParams = new GridLayout.LayoutParams(
+                GridLayout.spec(4),
+                GridLayout.spec(2,2)
+        );
+        col3txt = new TextView(this);
+        col3txt.setText("min <= hour && hour <= max");
+        col3txt.setGravity(Gravity.CENTER);
+        col3txt.setMinWidth(col3width);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col3txt.setLayoutParams(gParams);
+        root.addView(col3txt);
+
+    //col 3
+        gParams = new GridLayout.LayoutParams(
+                GridLayout.spec(5),
+                GridLayout.spec(2,1)
+        );
+        col3txt = new TextView(this);
+        col3txt.setText("int min");
+        col3txt.setGravity(Gravity.CENTER);
+        col3txt.setMinWidth(col3width/2);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col3txt.setLayoutParams(gParams);
+        root.addView(col3txt);
+
+        col3txt = new TextView(this);
+        col3txt.setText("From");
+        col3txt.setPadding(10,0,0,0);
+        col3txt.setMinWidth(col3width/2);
+        col3txt.setTypeface(null, Typeface.BOLD);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border_yellow));
+        root.addView(col3txt);
+
+        //Array of str numbers to populate cells
+        String[] nums = {"0","12","18","22","11","17","21","23"};
+        for (int i=0;i<4;++i){
+            col3txt = new TextView(this);
+            col3txt.setText(nums[i]);
+            col3txt.setGravity(Gravity.RIGHT);
+            col3txt.setPadding(0,0,10,0);
+            col3txt.setMinWidth(col3width/2);
+            col3txt.setBackground(getDrawable(R.drawable.thick_border_yellow));
+            root.addView(col3txt);
+        }
+
+        //col 4
+        gParams = new GridLayout.LayoutParams(
+                GridLayout.spec(5),
+                GridLayout.spec(3)
+        );
+        col3txt = new TextView(this);
+        col3txt.setText("int max");
+        col3txt.setGravity(Gravity.CENTER);
+        col3txt.setMinWidth(col3width/2);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col3txt.setLayoutParams(gParams);
+        root.addView(col3txt);
+
+        col3txt = new TextView(this);
+        col3txt.setText("To");
+        col3txt.setPadding(10,0,0,0);
+        col3txt.setMinWidth(col3width/2);
+        col3txt.setTypeface(null, Typeface.BOLD);
+        col3txt.setBackground(getDrawable(R.drawable.thick_border_yellow));
+        root.addView(col3txt);
+
+        for (int i=4;i<8;++i){
+            col3txt = new TextView(this);
+            col3txt.setText(nums[i]);
+            col3txt.setGravity(Gravity.RIGHT);
+            col3txt.setPadding(0,0,10,0);
+            col3txt.setMinWidth(col3width/2);
+            col3txt.setBackground(getDrawable(R.drawable.thick_border_yellow));
+            root.addView(col3txt);
+        }
+
+        //Column 5
 
 
+        TextView col5txt = new TextView(this);
+        col5txt.setText("Day Hour Classification");
+        col5txt.setBackground(getDrawable(R.drawable.thick_border));
+        col5txt.setPadding(10,0,0,0);
+        col5txt.setMinWidth(col5width);
+        root.addView(col5txt);
 
+        col5txt = new TextView(this);
+        col5txt.setText("Day and Time");
+        col5txt.setBackground(getDrawable(R.drawable.thick_border));
+        col5txt.setPadding(10,0,0,0);
+        col5txt.setMinWidth(col5width);
+        root.addView(col5txt);
 
-        //Adding to main grid view group "root" in order of column
+        col5txt = new TextView(this);
+        col5txt.setText("A1");
+        col5txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col5txt.setGravity(Gravity.CENTER);
+        col5txt.setTypeface(null, Typeface.BOLD);
+        col5txt.setMinWidth(col5width);
+        root.addView(col5txt);
 
+        col5txt = new TextView(this);
+        col5txt.setText("System.out.println(greeting + \",World!\"");
+        col5txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col5txt.setGravity(Gravity.CENTER);
+        col5txt.setMinWidth(col5width);
+        root.addView(col5txt);
+
+        col5txt = new TextView(this);
+        col5txt.setText("String greeting");
+        col5txt.setBackground(getDrawable(R.drawable.thick_border_cyan));
+        col5txt.setGravity(Gravity.CENTER);
+        col5txt.setMinWidth(col5width);
+        root.addView(col5txt);
+
+        col5txt = new TextView(this);
+        col5txt.setText("Greeting");
+        col5txt.setBackground(getDrawable(R.drawable.thick_border_orange));
+        col5txt.setTypeface(null, Typeface.BOLD);
+        col5txt.setPadding(10,0,0,0);
+        col5txt.setMinWidth(col5width);
+        root.addView(col5txt);
+
+        String[] phrases = {"Good Morning","Good Afternoon","Good Evening","Good Night"};
+        for (String x :phrases) {
+            col5txt = new TextView(this);
+            col5txt.setText(x);
+            col5txt.setBackground(getDrawable(R.drawable.thick_border_orange));
+            col5txt.setPadding(10, 0, 0, 0);
+            col5txt.setMinWidth(col5width);
+            root.addView(col5txt);
+        }
+        
         setContentView(root);
-
-
     }
 
 }
